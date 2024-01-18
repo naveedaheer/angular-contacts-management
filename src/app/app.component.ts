4f16e7e649e31cb94c2d9c628ab0a74925676054
Naveed Aheer <naveedaheerpc@gmail.com>
2024-01-18 11:36:39 +0500

    Updated title text

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-contacts-management';
+  title = 'Contacts Management App';
 }
