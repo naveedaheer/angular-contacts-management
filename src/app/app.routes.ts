6939a0ab410de055778e09c6875120d37dda583c
Naveed Aheer <naveedaheerpc@gmail.com>
2024-01-18 10:49:44 +0500

    Created route for contacts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { ContactsComponent } from '../contacts/contacts.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'contacts', component: ContactsComponent },
+    // { path: '**', component: NotFoundComponent }
+];
