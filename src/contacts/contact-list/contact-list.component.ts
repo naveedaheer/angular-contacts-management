ce6f5404728a51e456534987fde6f4d2b21ad797
Naveed Aheer <naveedaheerpc@gmail.com>
2024-01-18 19:24:10 +0500

    Created add/edit form

--- src/contacts/contact-list/contact-list.component.ts
+++ src/contacts/contact-list/contact-list.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-contact-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './contact-list.component.html',
-  styleUrl: './contact-list.component.scss'
+  styleUrl: './contact-list.component.scss',
 })
 export class ContactListComponent {
+  isHidden = true;
 
+  openModal() {
+    this.isHidden = false;
+  }
+
+  closeModal() {
+    this.isHidden = true;
+  }
 }
