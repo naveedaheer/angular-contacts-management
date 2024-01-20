c66ff7bb87d886000c5c0c621e067af280b2c826
Naveed Aheer <naveedaheerpc@gmail.com>
2024-01-20 19:49:32 +0500

    Update server.js
    updated path for heroku deployment

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/angular-contacts-management'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/angular-contacts-management/index.html'));
+res.sendFile(path.join(__dirname+'/dist/angular-contacts-management/browser/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
