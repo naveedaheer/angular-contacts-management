27411acb0d1faf75ab1b471f46aa187c78fa6409
Naveed Aheer <naveedaheerpc@gmail.com>
2024-01-21 17:12:29 +0500

    Connected store data to fetch contacts

--- src/app/contacts/contacts.component.ts
+++ src/app/contacts/contacts.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
-import { ContactListComponent } from "./contact-list/contact-list.component";
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { Observable } from 'rxjs';
+import { select, Store } from '@ngrx/store';
+import { AppState } from '../store/app.state';
+import { loadContacts } from '../store/state/contacts.actions';
+import { selectContacts } from '../store/state/contacts.selector';
+import { contact } from '../models/contact.model';
 
 @Component({
     selector: 'app-contacts',
@@ -9,5 +15,15 @@ import { ContactListComponent } from "./contact-list/contact-list.component";
     imports: [ContactListComponent]
 })
 export class ContactsComponent {
+    contacts$: Observable<contact[]>;
 
+    constructor(
+        private store: Store<AppState>,
+    ) {
+        this.contacts$ = this.store.pipe(select(selectContacts));
+    }
+
+    ngOnInit() {
+        this.store.dispatch(loadContacts());
+    }
 }
--- src/app/store/app.state.ts
+++ src/app/store/app.state.ts
@@ -1,8 +1,10 @@
 import { CONTACT_STATE_NAME } from '../store/state/contacts.selector';
 import { routerReducer, RouterReducerState } from '@ngrx/router-store';
 import { contactReducer } from '../store/state/contacts.reducer';
+import { ContactState } from './state/contacts.state';
 
 export interface AppState {
+  [CONTACT_STATE_NAME]: ContactState,
   router: RouterReducerState;
 }
 
