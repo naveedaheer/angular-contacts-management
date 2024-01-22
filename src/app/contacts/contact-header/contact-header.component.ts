3fd5d0a9b504751998ae52d9d986e5e9d5bba021
Naveed Aheer <naveedaheerpc@gmail.com>
2024-01-22 21:15:46 +0500

    implemented filter reset

--- src/app/contacts/contact-header/contact-header.component.ts
+++ src/app/contacts/contact-header/contact-header.component.ts
@@ -5,23 +5,32 @@ import { Contact } from 'src/app/models/contact.model';
 import { FormsModule } from '@angular/forms';
 
 @Component({
-    selector: 'app-contact-header',
-    standalone: true,
-    imports: [CommonModule, ContactFormComponent, FormsModule],
-    templateUrl: './contact-header.component.html',
-    styleUrls: ['./contact-header.component.scss']
+  selector: 'app-contact-header',
+  standalone: true,
+  imports: [CommonModule, ContactFormComponent, FormsModule],
+  templateUrl: './contact-header.component.html',
+  styleUrls: ['./contact-header.component.scss'],
 })
 export class ContactHeaderComponent {
-    @Output() addContactClicked = new EventEmitter<void>();
-    @Output() searchContacts = new EventEmitter<Contact>();
-    firstName: string = '';
-    lastName: string = '';
+  @Output() addContactClicked = new EventEmitter<void>();
+  @Output() searchContacts = new EventEmitter<Contact>();
+  firstName: string = '';
+  lastName: string = '';
 
-    onAddContactClick() {
-        this.addContactClicked.emit();
-    }
+  onAddContactClick() {
+    this.addContactClicked.emit();
+  }
 
-    onSearchContacts() {
-        this.searchContacts.emit({ firstName: this.firstName, lastName: this.lastName });
-    }
+  onSearchContacts() {
+    this.searchContacts.emit({
+      firstName: this.firstName,
+      lastName: this.lastName,
+    });
+  }
+
+  onResetContacts() {
+    this.firstName = '';
+    this.lastName = '';
+    this.searchContacts.emit({ firstName: '', lastName: '' });
+  }
 }
