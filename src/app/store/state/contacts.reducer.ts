88de725833527e72b6d17e336127c976ee9357e3
Naveed Aheer <naveedaheerpc@gmail.com>
2024-01-22 19:39:27 +0500

    display success and fail snackbar

--- src/app/store/state/contacts.reducer.ts
+++ src/app/store/state/contacts.reducer.ts
@@ -16,48 +16,59 @@ export const contactReducer = createReducer(
 
     on(deleteContact, (state) => ({ ...state, deleting: true })),
     on(deleteContactSuccess, (state, { contactId }) => {
-        showSnackbar("Contact deleted Successfully")
+        showSnackbar("Contact deleted Successfully", 'success')
         return {
             ...state,
             contacts: state.contacts.filter((contact) => contact.id !== contactId),
             deleting: false,
         }
     }),
-    on(deleteContactFailure, (state, { error }) => ({ ...state, error, deleting: false })),
+    on(deleteContactFailure, (state, { error }) => {
+        showSnackbar("Something went wrong", 'error')
+        return ({ ...state, error, updating: false })
+    }),
 
 
     on(addContact, (state) => ({ ...state, adding: true })),
     on(addContactSuccess, (state, { contact }) => {
-        showSnackbar("Contact Added Successfully")
+        showSnackbar("Contact Added Successfully", 'success')
         return {
             ...state,
             adding: false,
         }
     }),
-    on(addContactFailure, (state, { error }) => ({ ...state, error, adding: false })),
+    on(addContactFailure, (state, { error }) => {
+        showSnackbar("Something went wrong", 'error')
+        return ({ ...state, error, updating: false })
+    }),
 
     on(updateContact, (state) => ({ ...state, updating: true })),
     on(updateContactSuccess, (state, { contact }) => {
-        showSnackbar("Contact updated Successfully")
+        showSnackbar("Contact updated Successfully", 'success')
         return {
             ...state,
             updating: false,
         }
     }),
-    on(updateContactFailure, (state, { error }) => ({ ...state, error, updating: false }))
+    on(updateContactFailure, (state, { error }) => {
+        showSnackbar("Something went wrong", 'error')
+        return ({ ...state, error, updating: false })
+    })
 );
 
-function showSnackbar(message: string) {
+function showSnackbar(message: string, status: 'success' | 'error') {
     const snackbar = document.getElementById('snackbar');
     const snackbarText = document.getElementById('snackbar-text');
-
     if (snackbar && snackbarText) {
         snackbarText.textContent = message;
-        snackbar.classList.add('show');
-        setTimeout(() => {
-            snackbar.classList.remove('show');
-            snackbar.classList.add('hide')
 
+        snackbar.classList.toggle('hidden', status !== 'success' && status !== 'error');
+
+        snackbar.classList.toggle('bg-green-500', status === 'success');
+        snackbar.classList.toggle('bg-red-500', status === 'error');
+
+        setTimeout(() => {
+            snackbar.classList.add('hidden');
         }, 6000);
     }
 };
