42c0a4b97ebbbb9d0d8ee61b0ed448e6e38878a9
Naveed Aheer <naveedaheer@gmail.com>
2024-01-22 14:52:24 +0500

    Refactor edit contact form

--- src/app/contacts/contact-form/contact-form.component.ts
+++ src/app/contacts/contact-form/contact-form.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-contact-form',
-  standalone: true,
-  imports: [],
-  templateUrl: './contact-form.component.html',
-  styleUrls: ['./contact-form.component.scss']
+    selector: 'app-contact-form',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './contact-form.component.html',
+    styleUrls: ['./contact-form.component.scss']
 })
 export class ContactFormComponent {
+    @Input() isModalOpen: boolean = false
 
+    closeModal() {
+        this.isModalOpen = false
+    }
 }
--- src/app/contacts/contact-list/contact-list.component.ts
+++ src/app/contacts/contact-list/contact-list.component.ts
@@ -1,36 +1,36 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { Contact } from 'src/app/models/contact.model';
-import { EditContactComponent } from '../edit-contact/edit-contact.component';
+import { ContactFormComponent } from '../contact-form/contact-form.component';
 @Component({
-  selector: 'app-contact-list',
-  standalone: true,
-  imports: [CommonModule, EditContactComponent],
-  templateUrl: './contact-list.component.html',
-  styleUrls: ['./contact-list.component.scss'],
+    selector: 'app-contact-list',
+    standalone: true,
+    imports: [CommonModule, ContactFormComponent],
+    templateUrl: './contact-list.component.html',
+    styleUrls: ['./contact-list.component.scss'],
 })
 export class ContactListComponent {
-  @Input() contacts: Contact[] | null = [];
-  isModalOpen: boolean = false
-  isHidden = true;
-  isDelete = true;
+    @Input() contacts: Contact[] | null = [];
+    isModalOpen: boolean = false
+    isHidden = true;
+    isDelete = true;
 
-  openModal() {
-    console.log(this.isModalOpen)
-    this.isModalOpen = !this.isModalOpen
-    console.log(this.isModalOpen)
+    openModal() {
+        console.log(this.isModalOpen)
+        this.isModalOpen = !this.isModalOpen
+        console.log(this.isModalOpen)
 
-  }
+    }
 
-  closeModal() {
-    this.isHidden = true;
-  }
+    closeModal() {
+        this.isHidden = true;
+    }
 
-  openDeleteModal() {
-    this.isDelete = false;
-  }
+    openDeleteModal() {
+        this.isDelete = false;
+    }
 
-  closeDeleteModal() {
-    this.isDelete = true;
-  }
+    closeDeleteModal() {
+        this.isDelete = true;
+    }
 }
